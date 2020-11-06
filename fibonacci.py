e70998a99372bcfe61f0d23995abc315a80e2479
Puneet Kumar <55717028+puneku@users.noreply.github.com>
2020-11-06 14:15:57 +0530

    added exception case

--- fibonacci.py
+++ fibonacci.py
@@ -6,13 +6,16 @@ n = int(input("Enter a number\n"))
 def fibonacci(n):
     ft = 0 
     st = 1
-    print(ft)
-    print(st)
-    for i in range(2,n):
-        mt = ft + st
-        print(mt)
-        ft = st 
-        st = mt
+    if n == 1 :
+        print(ft)
+    else:
+        print(ft)
+        print(st)
+        for i in range(2,n):
+            mt = ft + st
+            print(mt)
+            ft = st 
+            st = mt
     
     
 fibonacci(n)
