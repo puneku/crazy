5192e16bf88e97648505a6d3e5581db31a80f583
puneku <puneetkumar3031@gmail.com>
2020-11-09 22:09:48 +0530

    added factorial functions
    Signed-off-by: puneku <puneetkumar3031@gmail.com>

--- factorial.py
+++ factorial.py
@@ -1,1 +1,21 @@
 print("Code to calculate factorial")
+
+x = int(input("Enter a number\n"))
+
+def facto(var):
+    multi = 1
+    for i in range(1, var+1):
+        multi *= i
+    print("factorial of {} is {} ".format(str(var), str(multi)))
+
+facto(x)
+
+# Using recursion
+def fact(arg):
+    if arg == 0:
+        return 1
+    else:
+        return arg * fact(arg-1)
+
+val = fact(x)
+print("factorial of {} is {}".format(x,val))
