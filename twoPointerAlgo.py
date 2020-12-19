212076fcd61d70d2cad7e72c6e871b3c4200a259
puneku <puneetkumar3031@gmail.com>
2020-12-19 11:09:47 +0530

    printing a triplete with sum is equal to 0.
    Signed-off-by: puneku <puneetkumar3031@gmail.com>

--- twoPointerAlgo.py
+++ twoPointerAlgo.py
@@ -1,26 +1,30 @@
-'''Problem : for a given array A[n] of n element. Find a triplete whose sum is 0 with time complexity of order O(n**2)'''
+'''Problem : for a given array A[n] of n element. Find a triplete whose sum is 0 with time complexity of order O(n*n).'''
 
+f , s = -1 , -1
 def findTripleteSum(A):
-	# writing a double pointer algorithm for two addition of two element.
-	A.sort()
-	for k in range(len(A)-2):
-		if doubleSum(A,-A[k]):
-			return True
-	return False
+    # writing a double pointer algorithm for two addition of two element.
+    A.sort()
+    global f,s
+    for k in range(len(A)-1):
+        if doubleSum(A,-A[k],k+1):
+            return (A[f], A[s],A[k])
 
-def doubleSum(a,x):
-	i , j = 0 , len(a)-1
-	while i<j:
-		if a[i]+a[j]<x:
-			i+=1
-		elif a[i]+a[j]>x:
-			j-=1
-		else:
-			return True
-	return False
+def doubleSum(a,x,i):
+    j = len(a)-1
+    while i<j:
+        if a[i]+a[j]<x:
+            i+=1
+        elif a[i]+a[j]>x:
+            j-=1
+        else:
+            global f,s
+            f = i
+            s = j
+            return True
+    return False
 
-a = [-4,-3,-2,-1,1,2,3,4,5]
-if findTripleteSum(a):
-    print("exists")
+a = [-4,-3,-2,-1,1,2,3,4]
+if findTripleteSum(a) == None:
+    print("No triplete found.")
 else:
-    print("Does not exist")
+    print(findTripleteSum(a))
